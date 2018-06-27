428182a694883bca5443b12d37de0e4e3e6d40fb
Ubuntu <ubuntu@ip-172-31-2-227.us-east-2.compute.internal>
2018-06-27 18:18:20 +0000

    веб-хуки

--- main.py
+++ main.py
@@ -1,10 +1,49 @@
 import telebot
+import cherrypy
 
-bot = telebot.TeleBot('576157976:AAEU2KatpbAISu1BV34qUQCPKP0pwN0pteg')
+token = '576157976:AAEU2KatpbAISu1BV34qUQCPKP0pwN0pteg'
 
-@bot.message_handler(content_types=["text"])
-def repeat_all_messages(message):
-    bot.send_message(message.chat.id, message.text)
+WEBHOOK_HOST = '18.191.222.70'
+WEBHOOK_PORT = 8443
+WEBHOOK_LISTEN = '0.0.0.0'
 
-if __name__ == '__main__':
-    bot.polling(none_stop=True)
+WEBHOOK_SSL_CERT = 'ssl/webhook_cert.pem'
+WEBHOOK_SSL_PRIV = 'ssl/webhook_pkey.pem'
+
+WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
+WEBHOOK_URL_PATH = "/%s/" % (token)
+
+bot = telebot.TeleBot(token)
+
+class WebhookServer(object):
+    @cherrypy.expose
+    def index(self):
+        if 'content-length' in cherrypy.request.headers and \
+                        'content-type' in cherrypy.request.headers and \
+                        cherrypy.request.headers['content-type'] == 'application/json':
+            length = int(cherrypy.request.headers['content-length'])
+            json_string = cherrypy.request.body.read(length).decode("utf-8")
+            update = telebot.types.Update.de_json(json_string)
+            bot.process_new_updates([update])
+            return ''
+        else:
+            raise cherrypy.HTTPError(403)
+
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def echo_message(message):
+    bot.reply_to(message, message.text)
+
+bot.remove_webhook()
+
+bot.set_webhook(url=WEBHOOK_URL_BASE + WEBHOOK_URL_PATH,
+                certificate=open(WEBHOOK_SSL_CERT, 'r'))
+
+cherrypy.config.update({
+    'server.socket_host': WEBHOOK_LISTEN,
+    'server.socket_port': WEBHOOK_PORT,
+    'server.ssl_module': 'builtin',
+    'server.ssl_certificate': WEBHOOK_SSL_CERT,
+    'server.ssl_private_key': WEBHOOK_SSL_PRIV
+})
+
+cherrypy.quickstart(WebhookServer(), WEBHOOK_URL_PATH, {'/': {}})
